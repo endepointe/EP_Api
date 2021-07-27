364757c38ee3aa4802b77e8062f2ddc6bb10f7cf
endepointe <endepointe@gmail.com>
2021-07-26 22:07:01 -0700

    ssdd

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -5,5 +5,12 @@ namespace EP_Api.Models
     public long Id { get; set; }
     public string Name { get; set; }
     public bool IsComplete { get; set; }
+    public string Secret { get; set; }
+  }
+  public class TodoItemDTO
+  {
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public bool IsComplete { get; set; }
   }
 }
